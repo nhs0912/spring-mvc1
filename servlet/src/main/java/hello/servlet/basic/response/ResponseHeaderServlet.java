c5d1af3c2ed15bea6a4d5d612379b2b3cdac914e
heeseok.noh <nhs0912@nate.com>
2023-04-28 12:26:41 +0900

    feat : response-header service 구현

--- servlet/src/main/java/hello/servlet/basic/response/ResponseHeaderServlet.java
+++ servlet/src/main/java/hello/servlet/basic/response/ResponseHeaderServlet.java
@@ -6,6 +6,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 @WebServlet(name = "responseHeaderServlet", urlPatterns = "/response-header")
 public class ResponseHeaderServlet extends HttpServlet {
@@ -15,7 +16,13 @@ public class ResponseHeaderServlet extends HttpServlet {
         response.setStatus(HttpServletResponse.SC_OK);
 
         //[response-headers]
-        response.setHeader("Content-Type", "text/plain");
+        response.setHeader("Content-Type", "text/plain;charset=utf-8");
         response.setHeader("Cache-Control", "no-cache, no-store,must-revalidate ");
+        response.setHeader("Pragma", "no-cache");
+        response.setHeader("my-header", "hello");
+
+        PrintWriter writer = response.getWriter();
+        writer.println("ok 좋아!");
+
     }
 }
